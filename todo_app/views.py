f59ecf206ee09f4e06ef980910126171dbe72540
abolfazlakbary <abolfazl.akbary132@gmail.com>
2023-08-20 18:17:28 +0330

    Create and Update set

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.shortcuts import render, reverse, redirect
+from django.views.generic import ListView, CreateView, UpdateView
 from .models import ToDoList, ToDoItem
 
 class ListListView(ListView):
@@ -15,3 +16,48 @@ class ItemListView(ListView):
         context["todo_list"] = ToDoList.objects.get(id=self.kwargs['list_id'])
         return context
     
+class ListCreate(CreateView):
+    model = ToDoList
+    fields = ['title']
+    def get_context_data(self):
+        context = super(ListCreate, self).get_context_data()
+        context["title"] = "Add a new list" 
+        return context
+
+class ItemCreate(CreateView):
+    model = ToDoItem
+    fields = [
+        "todo_list",
+        "title",
+        "description",
+        "due_date",
+    ]
+    def get_initial(self):
+        initial_data = super(ItemCreate, self).get_initial()
+        todo_list = ToDoList.objects.get(id=self.kwargs['list_id'])
+        initial_data['todo_list'] = todo_list
+        return initial_data
+    def get_context_data(self):
+        context = super(ItemCreate, self).get_context_data()
+        todo_list = ToDoList.objects.get(id=self.kwargs['list_id'])
+        context["todo_list"] = todo_list
+        context["title"] = "Create a new item"
+        return context
+    def get_success_url(self):
+        return reverse("list", args=[self.object.todo_list_id])
+
+class ItemUpdate(UpdateView):
+    model = ToDoItem
+    fields = [
+        "todo_list",
+        "title",
+        "description",
+        "due_date",
+    ]
+    def get_context_data(self):
+        context = super(ItemUpdate, self).get_context_data()
+        context["todo_list"] = self.object.todo_list
+        context["title"] = "Edit item"
+        return context
+    def get_success_url(self):
+        return reverse("list", args=[self.object.todo_list_id])
