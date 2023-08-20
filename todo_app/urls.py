adf59cee90ad722fc297c441b9c1eb4aeac7488b
abolfazlakbary <abolfazl.akbary132@gmail.com>
2023-08-20 15:06:13 +0330

    Read Set

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.ListListView.as_view(), name='index'),
+    path('<int:list_id>/', views.ItemListView.as_view(), name='list')
 ]
